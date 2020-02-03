ce75b6bfdf430530cf94c2f0b8667cb11c46ad29
David Mireles <dmireles1010@gmail.com>
2020-02-03 01:51:15 -0800

    Tinkering

--- App.js
+++ App.js
@@ -1,23 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {StyleSheet, Platform, Image, Text, View} from 'react-native';
-import {SwitchNavigator} from 'react-navigation';
+import {createSwitchNavigator, createAppContainer} from 'react-navigation';
 // import the different screens
 import Loading from './src/pages/Loading';
-import SignUp from './src/pages/SignUp';
+import SignUp from './src/pages/Signup';
 import Login from './src/pages/Login';
 import Main from './src/pages/Main';
 import Map from './src/pages/Map';
 // create our app's navigation stack
-const App = SwitchNavigator(
-  {
-    Loading,
-    SignUp,
-    Login,
-    Main,
-    Map,
-  },
-  {
-    initialRouteName: 'Loading',
-  },
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      Loading: Loading,
+      Signup: SignUp,
+      Login: Login,
+      Main: Main,
+      Map: Map,
+    },
+    {
+      initialRouteName: 'Loading',
+    },
+  ),
 );
-export default App;
