732539d9acfa10e37dce02a94b76d201713182df
Mark Tucker <martuck3@yahoo.com>
2020-02-10 10:28:40 -0800

    Added User Welcome page

---
+++ src/components/Routes.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {Router, Stack, Scene} from 'react-native-router-flux';
+
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
+import Map from '../pages/Map'
+import Edit_profile from '../pages/Edit_Profile'
+import Profile_Welcome from '../pages/Profile_Welcome'
+
+export default class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Stack key="root" hideNavBar={true}>
+          <Scene key="login" component={Login} title="Login"  initial={true}/>
+          <Scene key="signup" component={Signup} title="Register" />
+          <Scene key="map" component={Map} title="Map"  />
+          <Scene key="Edit_Profile" component={Edit_profile} title="Edit_Profile"   />
+          <Scene key="Profile_Welcome" component={Profile_Welcome} title="Profile_Welcome"   />
+        </Stack>
+      </Router>
+    );
+  }
+}
